046415deb519c8b8c3a5cf872e02862d5feed502
gabaaa <not-set>
2019-04-12 11:45:20 -0500

    Bump Release Version 0.1.0

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 # version: 0.1.0
 # name: shell_helpers
 # license: MIT
-__version__ = "{version}"
+__version__ = "0.1.0"
 
 from .dev import shell_helpers as shell
 from .gpkgs import message as msg
