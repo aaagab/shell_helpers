9bbad6eb20f479cdf8f72e9826c816f3ffe77466
gabaaa <gabaaa.dev@gmail.com>
2019-04-12 11:53:05 -0500

    Correction

--- test.py
+++ test.py
@@ -9,28 +9,28 @@ import importlib
 direpa_script_parent=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 module_name=os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, direpa_script_parent)
-pkg = importlib.import_module(module_name)
+shell = importlib.import_module(module_name)
 del sys.path[0]
 
 cmd="ls -l"
 
-pkg.msg.info("execute command '{}' WITH output.".format(cmd))
-pkg.shell.cmd(cmd)
+shell.msg.info("execute command '{}' WITH output.".format(cmd))
+shell.cmd(cmd)
 print()
 
-pkg.msg.info("execute command '{}' WITHOUT output.".format(cmd))
-pkg.shell.cmd_devnull(cmd)
+shell.msg.info("execute command '{}' WITHOUT output.".format(cmd))
+shell.cmd_devnull(cmd)
 print()
 
-pkg.msg.info("command returns exit code '{}'".format(cmd))
-print(pkg.shell.cmd_devnull(cmd))
+shell.msg.info("command returns exit code '{}'".format(cmd))
+print(shell.cmd_devnull(cmd))
 print()
 
-pkg.msg.info("get command '{}' output in variable.".format(cmd))
-output=pkg.shell.cmd_get_value(cmd)
+shell.msg.info("get command '{}' output in variable.".format(cmd))
+output=shell.cmd_get_value(cmd)
 print(output)
 print()
 
-pkg.msg.info("verbose command '{}' mode.".format(cmd))
-pkg.shell.cmd_prompt(cmd, True)
+shell.msg.info("verbose command '{}' mode.".format(cmd))
+shell.cmd_prompt(cmd, True)
 print()
