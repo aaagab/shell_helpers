1bcf593c82e4d476c8d7401fcbf1f03bc6b2a668
gabaaa <gabaaa.dev@gmail.com>
2019-08-02 11:08:23 -0500

    Ported to windows

--- dev/shell_helpers.py
+++ dev/shell_helpers.py
@@ -4,6 +4,8 @@
 # name: shell_helpers
 # license: MIT
 
+import platform
+import shutil
 import subprocess
 import sys
 import shlex
@@ -13,22 +15,30 @@ import sys, os
 
 from ..gpkgs import message as msg
 
+def get_split_cmd(command):
+    split_cmd=shlex.split(command)
+    if platform.system() == "Windows":
+        if shutil.which(split_cmd[0]) is None:
+            split_cmd.insert(0, "cmd")
+            split_cmd.insert(1, "/c")
+    return split_cmd
+
 def cmd_devnull(command):
     try:
-        return subprocess.check_call(shlex.split(command), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return subprocess.check_call(get_split_cmd(command), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except subprocess.CalledProcessError as proc:
         return proc.returncode
 
 # ideal for any command with prompt
 def cmd(command):
     try:
-        return subprocess.call(shlex.split(command), shell=False)
+        return subprocess.call(get_split_cmd(command), shell=False)
     except subprocess.CalledProcessError as proc:
         return proc.returncode
 
 def cmd_get_value(command):
     try:
-        process = subprocess.Popen(shlex.split(command), 
+        process = subprocess.Popen(get_split_cmd(command), 
                                     shell=False, 
                                     stdout=subprocess.PIPE, 
                                     stderr=subprocess.PIPE)
@@ -45,6 +55,7 @@ def cmd_get_value(command):
             sys.exit(1)
     except Exception as e:
         frame,filename,line_number,function_name,lines,index=inspect.stack()[1]
+        print(e)
         print("\t"+str(line_number)+": "+filename)
         msg.error("Command: '"+command)
         sys.exit(1)
