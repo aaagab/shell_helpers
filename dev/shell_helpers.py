5677377c083bd19a20b103f89c2fc4309d2e2d48
gabaaa <not-set>
2019-08-02 10:48:38 -0500

    modified test for windows, upgraded message

--- dev/shell_helpers.py
+++ dev/shell_helpers.py
@@ -41,12 +41,12 @@ def cmd_get_value(command):
         else:
             frame,filename,line_number,function_name,lines,index=inspect.stack()[1]
             print("\t"+str(line_number)+": "+filename)
-            msg.app_error("Command: '"+command+"', err: "+stderr.decode("utf-8"))
+            msg.error("Command: '"+command+"', err: "+stderr.decode("utf-8"))
             sys.exit(1)
     except Exception as e:
         frame,filename,line_number,function_name,lines,index=inspect.stack()[1]
         print("\t"+str(line_number)+": "+filename)
-        msg.app_error("Command: '"+command)
+        msg.error("Command: '"+command)
         sys.exit(1)
 
 def cmd_prompt(cmd_txt, prompt_msg=False):
@@ -56,5 +56,5 @@ def cmd_prompt(cmd_txt, prompt_msg=False):
     if cmd(cmd_txt) == 0:
         msg.success(cmd_txt)
     else:
-        msg.app_error(cmd_txt +" failed!")
+        msg.error(cmd_txt +" failed!")
         sys.exit(1)
