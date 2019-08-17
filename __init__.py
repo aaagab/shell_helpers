603d5321849a08c95da2851009f1a22b6c57f194
gabaaa <gabaaa.dev@gmail.com>
2019-08-17 08:17:51 -0500

    Bump Release Version 5.0.0

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.0.1
+# version: 5.0.0
 # name: shell_helpers
 # license: MIT
-__version__ = "4.0.1"
+__version__ = "5.0.0"
 
 from .dev.shell_helpers import cmd, cmd_get_value, cmd_prompt, cmd_devnull
 from .gpkgs import message as msg
